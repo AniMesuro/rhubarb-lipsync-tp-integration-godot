3f601925aee0807d9c1c2832e8316ffb27ce2153
AniMesuro <64933585+AniMesuro@users.noreply.github.com>
2020-08-24 21:25:49 -0300

    Only create an animation keyframe if there's no animation on 0.0 time

--- addons/rhubarb_lipsync_integration/plugin.gd
+++ addons/rhubarb_lipsync_integration/plugin.gd
@@ -460,11 +460,14 @@ func import_lipsync(
 			 lipsync_start_time + float(sample[0]),
 			 mouthshape, 0)
 		elif is_instance_valid(mouthAnimSprite):
-			anim.track_insert_key(
-					tr_mouth_anim,
-					0.0,
-					mouthAnimSprite_anim
-			)
+			if anim.track_find_key(tr_mouth_anim, 0.0) == -1:
+				anim.track_insert_key(
+						tr_mouth_anim,
+						0.0,
+						mouthAnimSprite_anim
+				)
+			
+			
 			var mouthshape_frame :int= 0
 			# In MouthFrames mouthDB is based on ints instead of StreamTextures
 			match sample[1]:
